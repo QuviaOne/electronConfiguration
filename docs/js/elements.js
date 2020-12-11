4dc37f9cae15e48c59c5fce5a887a8676ac4abd1
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2020-12-11 19:20:36 +0100

    elements update

--- docs/js/elements.js
+++ docs/js/elements.js
@@ -1,3 +1,12 @@
 const elemetns = [
-    
+    {
+        symbol: "H",
+        name: "Hydrogen",
+        czech: "Vodík"
+    },
+    {
+        symbol: "He",
+        name: "Helium",
+        czech: "Helium"
+    }
 ]
